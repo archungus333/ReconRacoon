66d58061120e1e1ae1a6be6030e55e05b795c2ee
whatev3r <113145908+archungus333@users.noreply.github.com>
2023-02-02 10:03:58 +0100

    reverse test

--- reconracoon.py
+++ reconracoon.py
@@ -5,12 +5,6 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 requests.adapters.DEFAULT_RETRIES = 100
 
-
-
-TEST
-
-
-
 # Ascii banner
 racoon = r'''
                         ,,,
