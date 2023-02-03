3a3617a4bd421e8e70a7560683eb66095af76381
f0xg0d <gunnery.brand@googlemail.com>
2023-02-03 14:20:53 +0100

    Update reconracoon.py

--- reconracoon.py
+++ reconracoon.py
@@ -37,9 +37,9 @@ parser.add_argument('-t', '--target', dest='target', type=str, required=True, he
 parser.add_argument('-d', '--delay', dest='timeout', type=float, default=1.0, help='Timeout for all web requests')
 parser.add_argument('-u', '--user-agent', dest='user_agent', default='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36', type=str, help='Use custom user agent')
 parser.add_argument('-c', '--common-ports', action='store_true', help='Check all common webserver ports (seclist)')
+parser.add_argument('-r', '--robots', action='store_true', help='Saves the robots.txt of each target in a folder (robots/)')
 parser.add_argument('-v', '--verbose', action='store_true', help='Display verbose output (timeouts/errors)')
 #parser.add_argument('-s', '--screenshot', action='store_true', help='Takes a screenshot of the webpage and stores it in screenshots/')
-parser.add_argument('-r', '--robots', action='store_true', help='Saves the robots.txt in robots/')
 args = parser.parse_args()
 
 # Common http ports
