1940a4db6b75c33a8ef1740e4ee80b0beed1cab8
whatev3r <113145908+archungus333@users.noreply.github.com>
2023-02-15 14:36:51 +0100

    Create cli.py

---
+++ bin/framework/cli.py
@@ -0,0 +1,28 @@
+# IMPORT
+import datetime
+
+# COLORS
+endc = '\033[m'
+black = '\033[30m'
+red = '\033[31m'
+green = '\033[32m'
+yellow = '\033[33m'
+blue = '\033[34m'
+purple = '\033[35m'
+cyan = '\033[36m'
+white = '\033[37m'
+
+# TS
+now = datetime.datetime.now()
+current_time = now.strftime("%H:%M:%S")
+current_date = now.strftime("%D")
+
+# ASCII
+racoon = rf'''┌───────────────────────────────────────────┐
+│ DT: {current_date}           ,,,                │
+│ TS: {current_time}        .'    `/\_/\          │
+│                   .'       <{yellow}─{endc}I{purple}─{endc}>          │
+│        <((((((((((  )____(  \./           │
+│                   \( \(   \(\(            │
+│ {yellow}Recon{purple}Racoon{endc}        `-"`-"  " "            │
+└───────────────────────────────────────────┘'''
